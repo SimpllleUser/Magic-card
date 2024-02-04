76b1b949bd64b0b56c07f77afc05ddb403c63ae0
v.pecheritsya <v.pecheritsya@upstars.com>
2024-02-04 22:35:00 +0200

    feat: add empty state list'

--- src/modules/module/pages/ModuleList.vue
+++ src/modules/module/pages/ModuleList.vue
@@ -1,20 +1,28 @@
 <script lang="ts" setup>
-  import { useModulesStore } from 'src/modules/module/store/modules';
   import { computed } from 'vue';
-  import VModal from 'components/VModal.vue';
-  import { useModal } from 'src/composables/useModal';
+  import ModuleItem from 'src/modules/module/components/ModuleItem.vue';
 
-  const modulesStore = useModulesStore();
+  import { useModulesStore } from 'src/modules/module/store/modules';
 
-  const listOfModules = computed(() => modulesStore.modules);
-
-  const testModal = useModal('test');
+  const moduleStore = useModulesStore();
+  const modules = computed(() => moduleStore.modules);
 </script>
 
 <template>
-  <div>
-    <h4>Modules list</h4>
-    {{ listOfModules }}
+  <div class="q-px-xs">
+    <div v-if="modules.length" class="row">
+      <div v-for="module in modules" :key="module.id" class="col-4">
+        <module-item />
+      </div>
+    </div>
+    <div v-else>
+      <div><h4 class="text-blue-grey-8 text-center">You can add new module</h4></div>
+      <div class="row justify-center">
+        <div class="col-3 row justify-center">
+          <q-btn color="secondary" text-color="black" icon="add" style="font-size: 5rem"></q-btn>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
