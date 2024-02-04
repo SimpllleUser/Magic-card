0af4824bad8e2cca07d3f052fff2bd7f9c586710
v.pecheritsya <v.pecheritsya@upstars.com>
2024-02-04 19:40:52 +0200

    feat: add module list

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,15 +1,18 @@
 import { RouteRecordRaw } from 'vue-router';
 
+import moduleROuter from '../modules/module/routes';
+
 const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }]
-  },
+  // {
+  //   path: '/',
+  //   component: () => import('layouts/MainLayout.vue'),
+  //   children: [{ path: '', component: () => import('pages/IndexPage.vue') }]
+  // },
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
-  }
+  },
+  ...moduleROuter
 ];
 
 export default routes;
