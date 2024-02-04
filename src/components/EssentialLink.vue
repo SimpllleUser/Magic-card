738690e46f2aed5ebe13f4872e81bb553bb77c8c
v.pecheritsya <v.pecheritsya@upstars.com>
2024-02-04 19:10:37 +0200

    fix eslint files

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,14 +1,6 @@
 <template>
-  <q-item
-    clickable
-    tag="a"
-    target="_blank"
-    :href="link"
-  >
-    <q-item-section
-      v-if="icon"
-      avatar
-    >
+  <q-item clickable tag="a" target="_blank" :href="link">
+    <q-item-section v-if="icon" avatar>
       <q-icon :name="icon" />
     </q-item-section>
 
@@ -20,15 +12,15 @@
 </template>
 
 <script setup lang="ts">
-export interface EssentialLinkProps {
-  title: string;
-  caption?: string;
-  link?: string;
-  icon?: string;
-}
-withDefaults(defineProps<EssentialLinkProps>(), {
-  caption: '',
-  link: '#',
-  icon: '',
-});
+  export interface EssentialLinkProps {
+    title: string;
+    caption?: string;
+    link?: string;
+    icon?: string;
+  }
+  withDefaults(defineProps<EssentialLinkProps>(), {
+    caption: '',
+    link: '#',
+    icon: ''
+  });
 </script>
