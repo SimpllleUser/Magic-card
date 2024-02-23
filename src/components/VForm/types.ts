fedbd8a344095a7d335543d14c4f18e4f100fba0
v.pecheritsya <v.pecheritsya@upstars.com>
2024-02-23 22:52:29 +0200

    feat(MC-1):connect enums to rules key and array of rules

--- src/components/VForm/types.ts
+++ src/components/VForm/types.ts
@@ -1,5 +1,5 @@
 type SelectValue = string | Record<string, unknown>;
-type BaseInput<T> = { value: T; rules: Array<string> };
+type BaseInput<T> = { value: T; rules: Array<ValidationRule> };
 type BaseForm<T> = T & { error: string };
 
 export type InputItem = BaseInput<string>;
@@ -13,6 +13,38 @@ export type FormSelectItem = BaseForm<SelectItem>;
 export type FormCheckItem = BaseForm<CheckItem>;
 
 export type FormItemConfig = FormInputItem | FormSelectItem | FormCheckItem;
+
+export enum ValidationRule {
+  After = 'after',
+  Alpha = 'alpha',
+  AlphaDash = 'alpha_dash',
+  AlphaNum = 'alpha_num',
+  AlphaSpaces = 'alpha_spaces',
+  Before = 'before',
+  Between = 'between',
+  Confirmed = 'confirmed',
+  CreditCard = 'credit_card',
+  DateBetween = 'date_between',
+  Decimal = 'decimal',
+  Digits = 'digits',
+  Image = 'image',
+  Included = 'included',
+  Integer = 'integer',
+  IP = 'ip',
+  IPOrFQDN = 'ip_or_fqdn',
+  Is = 'is',
+  IsNot = 'is_not',
+  Length = 'length',
+  Max = 'max',
+  Min = 'min',
+  Mimes = 'mimes',
+  Numeric = 'numeric',
+  Regex = 'regex',
+  Required = 'required',
+  RequiredIf = 'required_if',
+  Size = 'size',
+  URL = 'url'
+}
 
 // export enum InputTypes {
 //   Input = 'input',
--- src/modules/module/components/ModuleForm.vue
+++ src/modules/module/components/ModuleForm.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-  import { ref } from 'vue';
   import VModal from 'components/VModal.vue';
   import VInput from 'components/VForm/VInput/VInput.vue';
 
   import { useForm } from 'components/VForm/composables/useForm';
-
-  const name = ref('');
-  // const flag = ref(false);
-  // const select = ref(3);
+  import { ValidationRule } from 'components/VForm/types';
 
   const { formData, onSubmit, onReset } = useForm({
-    name: {
-      value: 'Some name',
-      rules: ['alpha_spaces']
+    title: {
+      value: '',
+      rules: [ValidationRule.Required]
+    },
+    description: {
+      value: '',
+      rules: [ValidationRule.Required]
     }
   });
 
@@ -28,6 +28,7 @@
   <v-modal id="form-module" title="Module form">
     <div>
       <VInput v-model="formData.name" />
+      <VInput v-model="formData.name" type="textarea" />
       <q-btn @click="onSave">Save</q-btn>
       <q-btn @click="onsReset">Reset</q-btn>
     </div>
