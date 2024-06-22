c86d8984a658478b5f5e5662df182e620d405cbb
v.pecheritsya <v.pecheritsya@upstars.com>
2024-06-22 23:10:34 +0300

    refactor: add base refactor of word list

--- src/features/words/components/WordsList.vue
+++ src/features/words/components/WordsList.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-  import { ref, defineProps, computed } from 'vue';
+  import { ref, defineProps, watch } from 'vue';
   import { WordEntity } from '../types/word';
-
-  import { useCRUD } from 'src/shared/composables/useCRUD';
-  import { findIndex } from 'lodash';
 
   interface Props {
     words: Array<WordEntity>;
@@ -17,77 +14,25 @@
 
   const emit = defineEmits<Emit>();
 
-  const initWordsCRUD = () => useCRUD<WordEntity>(props.words, { returnAsObject: true });
-  const removeWord = (word: WordEntity) => {
-    const listCrud = initWordsCRUD();
-    listCrud.remove(word.id);
+  const rows = ref(props.words);
+
+  const onRemoveWord = (wordId: string) => {
+    rows.value = rows.value.filter(({ id }) => id !== wordId);
   };
 
-  const editableWorld = ref<WordEntity | null>(null);
-  const editWord = (word: WordEntity) => {
-    editableWorld.value = word;
-  };
+  const getEditableWordItem = (word: WordEntity, index: number) => ({
+    ...word,
+    index
+  });
 
-  const disabledEdit = () => {
-    editableWorld.value = null;
-  };
-
-  const activeEditModeAction = [
-    {
-      icon: 'check',
-      rounded: true,
-      size: 'sm',
-      click: (word) => {
-        const listCrud = initWordsCRUD();
-        listCrud.update(word);
-        emit('update-list', listCrud.data.value);
-        disabledEdit();
-      },
-      color: 'positive'
+  watch(
+    () => props.words,
+    () => {
+      rows.value = props.words.map(getEditableWordItem);
     },
     {
-      icon: 'cancel',
-      rounded: true,
-      size: 'sm',
-      click: (word) => {
-        const listCrud = initWordsCRUD();
-        const wordIndex = findIndex(props.words, { id: word.id });
-        listCrud.update(props.words[wordIndex]);
-        disabledEdit();
-      },
-      color: 'negative',
-      class: 'q-ml-sm'
+      immediate: true
     }
-  ];
-
-  const callableAction = (word: WordEntity, action?: CallableFunction) => () => {
-    action && action(word);
-  };
-
-  const rows = computed(() =>
-    props?.words
-      ?.map((word: WordEntity, index: number) => ({ ...word, index, number: index + 1 }))
-      ?.map((word: WordEntity) => ({
-        ...word,
-        actions: [
-          {
-            click: callableAction(word, editWord),
-            icon: 'edit',
-            color: 'primary',
-            unelevated: true,
-            size: 'sm',
-            rounded: true
-          },
-          {
-            click: callableAction(word, removeWord),
-            icon: 'delete',
-            color: 'primary',
-            outline: true,
-            size: 'sm',
-            rounded: true
-          }
-        ]
-      }))
   );
 
   const COLUMNS = [
@@ -107,8 +52,6 @@
     noDataLabel: 'Empty list of words',
     noResultsLabel: 'Not find of words'
   };
-
-  const isEditableWord = (word: WordEntity) => editableWorld.value?.id === word?.id;
 </script>
 
 <template>
@@ -116,35 +59,27 @@
     <q-table :rows="rows" v-bind="TABLE_CONFIG">
       <template v-slot:body-cell-from="props">
         <q-td :props="props">
-          <div v-if="!isEditableWord(props.row)">{{ props.row.from }}</div>
-          <q-input v-else v-model="editableWorld.from" />
+          <q-input v-model="rows[props.row.index].from" />
         </q-td>
       </template>
       <template v-slot:body-cell-to="props">
         <q-td :props="props">
-          <div v-if="!isEditableWord(props.row)">{{ props.row.to }}</div>
-          <q-input v-else v-model="editableWorld.to" />
+          <q-input v-model="rows[props.row.index].to" />
         </q-td>
       </template>
       <template v-slot:body-cell-actions="props">
         <q-td :props="props">
           <div class="actions row justify-end">
-            <div v-if="isEditableWord(props.row)">
-              <q-btn
-                v-for="(actionEdit, index) in activeEditModeAction"
-                :key="index"
-                v-bind="actionEdit"
-                @click="actionEdit.click(editableWorld)"
-              />
-            </div>
-            <div
-              v-else
-              class="actions__item q-ml-sm row items-center"
-              v-for="action in props.row.actions"
-              :key="action.icon"
-            >
-              <q-btn v-bind="action" @click="action.click" />
-            </div>
+            <q-btn
+              v-bind="{
+                icon: 'delete',
+                color: 'primary',
+                outline: true,
+                size: 'sm',
+                rounded: true
+              }"
+              @click="onRemoveWord(props.row.id)"
+            />
           </div>
         </q-td>
       </template>
