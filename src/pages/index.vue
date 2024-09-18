a3ca038961efed9723711658d0e5c28349e50365
Vitalii <pecheritsa.vv@gmail.com>
2024-09-18 22:20:01 +0300

    Feature init base topic

---
+++ src/pages/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+  import TopicCard from '@/pages/Topics/components/TopicCard.vue';
+  import { useTopicsStore } from '@/pages/Topics/store/topics';
+
+  const topicsStore = useTopicsStore();
+</script>
+
+<template>
+  <div>List of card Topics</div>
+  <div>
+    <VRow class="pa-4">
+      <VCol v-for="topic in topicsStore.items" :key="topic.id" cols="4">
+        <TopicCard :topic="topic" />
+      </VCol>
+    </VRow>
+  </div>
+</template>
