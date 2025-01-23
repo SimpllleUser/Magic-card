6e36eecf97e4d14a62d533dbd2f85223c4b2903f
Vitalii <pecheritsa.vv@gmail.com>
2025-01-23 21:39:26 +0200

    Feat: add base animation on switch page

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -6,9 +6,22 @@
   <v-app>
     <DefaultHeader />
     <v-main class="px-4 mt-4">
-      <!-- TODO add breadcramp -->
-      <router-view />
+      <transition name="scale" mode="out-in">
+        <router-view />
+      </transition>
     </v-main>
     <AppFooter />
   </v-app>
 </template>
+<style>
+  .scale-enter-active,
+  .scale-leave-active {
+    transition: all 0.5s ease;
+  }
+
+  .scale-enter-from,
+  .scale-leave-to {
+    opacity: 0;
+    transform: scale(0.9);
+  }
+</style>
