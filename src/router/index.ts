783e5b27f4f35c191ffdb1cf820f9e04fbe85b18
Vitalii <pecheritsa.vv@gmail.com>
2025-03-26 23:58:51 +0200

    final fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import { routes } from './routes';
 
 const router = createRouter({
-  history: createWebHistory('/'),
+  history: createWebHistory(import.meta.env.PROD ? '/MagicCard/' : import.meta.env.BASE_URL),
   routes
 });
 
