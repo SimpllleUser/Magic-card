28cb8d54ee92ea8bb7ea7643c95755ca5b9cae5d
Vitalii <pecheritsa.vv@gmail.com>
2025-07-11 21:41:41 +0300

    fix: export to txt

--- src/shared/utils/exportData/toTxt.ts
+++ src/shared/utils/exportData/toTxt.ts
@@ -1,8 +1,5 @@
 export function toTxt(data: Record<string, any>[]): string {
   if (!data || data.length === 0) return '';
 
-  const headers = Object.keys(data[0]);
-  const rows = data.map((item) => headers.map((key) => `${key}: ${item[key] ?? ''}`).join('\n'));
-
-  return rows.join('\n\n');
+  return data.map((item) => `${item.from} - ${item.to}`).join(',\n');
 }
