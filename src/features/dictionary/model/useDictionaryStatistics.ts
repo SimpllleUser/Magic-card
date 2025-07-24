9eb82b1882f5e4b0c747507cd90a473c71ab12df
Vitalii <pecheritsa.vv@gmail.com>
2025-07-24 23:24:43 +0300

    refactor: separate calculation function

--- src/features/dictionary/model/useDictionaryStatistics.ts
+++ src/features/dictionary/model/useDictionaryStatistics.ts
@@ -1,5 +1,6 @@
 import { useCRUD } from '@/shared/use/useCRUD';
-import { CalculatedStatistic, DictionaryStatisticPrams, DictionaryStatistics } from '@/features/dictionary/model/types';
+import { DictionaryStatisticPrams, DictionaryStatistics } from '@/features/dictionary/model/types';
+import { getCalculationStatistics } from '@/features/dictionary/model/utils';
 
 // export type LevelOfKnowledge = 'learned' | 'wordNeedsAdditionalRepetition';
 /*
@@ -18,28 +19,13 @@ export function useDictionaryStatistics() {
 
   const statisticsCrud = useCRUD<DictionaryStatistics>([], { key: 'Dictionary-statistics', returnAsObject: true });
 
-  const getCalculationStatistics = (params: DictionaryStatisticPrams): CalculatedStatistic => {
-    const totalNumberOFAttempts = getTotalNumberOFAttempts(params.dictionaryId);
-    const IA = params.incorrectAnswers;
-    const CA = params.correctAnswers;
-    const TQ = params.totalQuestions;
-    const TT = params.timeTaken;
-
-    const ACC = (CA / TQ) * 100;
-    const ATQ = TT / TQ;
-    const WM = (CA / totalNumberOFAttempts) * 100; /// if >= 80 learned; if <= 50 wordNeedsAdditionalRepetition;
-
-    const ES = ACC * (1 / ATQ); /// Efficiency Score (ES) Поєднує точність і швидкість then higher then better
-
-    return { IA, ACC, ATQ, WM, ES };
-  };
-
   const getTotalNumberOFAttempts = (id: string): number =>
-    statisticsCrud.data.value.filter((item) => item.dictionaryId === id)?.length || 0 + 1;
+    statisticsCrud.data.value.filter((item) => item.dictionaryId === id)?.length + 1;
 
   const saveStatistics = (params: Omit<DictionaryStatisticPrams, 'ts'>) => {
+    const totalNumberOFAttempts = getTotalNumberOFAttempts(params.dictionaryId);
     const data: DictionaryStatistics = {
-      ...getCalculationStatistics(params),
+      ...getCalculationStatistics(params, totalNumberOFAttempts),
       id: new Date().valueOf().toString(),
       ts: new Date().valueOf(),
       dictionaryId: params.dictionaryId,
--- src/features/dictionary/model/utils.ts
+++ src/features/dictionary/model/utils.ts
@@ -1,6 +1,6 @@
 import { omit } from 'lodash';
 import { generateId } from '@/shared/utils/id-generate';
-import { Dictionary, DictionaryItem } from '../model/types';
+import { CalculatedStatistic, Dictionary, DictionaryItem, DictionaryStatisticPrams } from '../model/types';
 import { ENTITY_API_KEYS } from '@/shared/api/constants';
 
 export const mappedDictionaryItems = (dictionaryParams: Dictionary): Dictionary => ({
@@ -16,3 +16,21 @@ export const mappedDictionaryItems = (dictionaryParams: Dictionary): Dictionary 
 });
 
 export const makeCopyDictionary = (dictionary: Dictionary): Dictionary => omit(dictionary, ['id', ...ENTITY_API_KEYS]);
+
+export const getCalculationStatistics = (
+  params: DictionaryStatisticPrams,
+  totalNumberOFAttempts: number
+): CalculatedStatistic => {
+  const IA = params.incorrectAnswers;
+  const CA = params.correctAnswers;
+  const TQ = params.totalQuestions;
+  const TT = params.timeTaken;
+
+  const ACC = (CA / TQ) * 100;
+  const ATQ = TT / TQ;
+  const WM = (CA / totalNumberOFAttempts) * 100; /// if >= 80 learned; if <= 50 wordNeedsAdditionalRepetition;
+
+  const ES = ACC * (1 / ATQ); /// Efficiency Score (ES) Поєднує точність і швидкість then higher then better
+
+  return { IA, ACC, ATQ, WM, ES };
+};
