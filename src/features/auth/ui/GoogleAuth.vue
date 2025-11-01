083cdccb51656451fffbade971055019b8e92d20
Vitalii <pecheritsa.vv@gmail.com>
2025-11-01 20:51:38 +0200

    feat: enhance `GoogleAuth` with user retrieval and display logic
    - Added `onMounted` logic to fetch and display user data upon login.
    - Updated `loginWithGoogle` method to use `await` for improved session handling.
    - Introduced a conditional user display in the template.

--- src/features/auth/ui/GoogleAuth.vue
+++ src/features/auth/ui/GoogleAuth.vue
@@ -9,11 +9,29 @@
   defineOptions({ name: 'GoogleAuth' });
 
   const loginWithGoogle = () => {
-    account.createOAuth2Session(OAuthProvider.Google, 'https://magic-card.fun', 'https://magic-card.fun/login?error=1');
+    // account.createOAuth2Session(OAuthProvider.Google, 'http://localhost:3000/', 'http://localhost:3000//login?error=1');
+    await account.createOAuth2Session(
+      OAuthProvider.Google,
+      'https://magic-card.fun',
+      'https://magic-card.fun/login?error=1'
+    );
   };
+
+  const user = ref<any>();
+  onMounted(async () => {
+    const result = await account.get();
+    console.log(result);
+    user.value = result;
+  });
 </script>
 
 <template>
+  <div
+    v-if="user"
+    class="text-h6 text-center"
+  >
+    {{ user?.name }}
+  </div>
   <div>
     <VBtn @click="loginWithGoogle"> Sign in with Google </VBtn>
   </div>
